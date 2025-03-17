cf50b67b373f6eaff17c04d7d322a7785d64a1c8
Igor Tessaro <--global>
2025-03-17 10:11:32 -0300

    feat(expenses): enhance UpdateExpenseGroupValidator with existence checks for expense groups and users

--- src/SmartExpenseControl.Application/Expenses/Validators/UpdateExpenseGroupValidator.cs
+++ src/SmartExpenseControl.Application/Expenses/Validators/UpdateExpenseGroupValidator.cs
@@ -1,12 +1,20 @@
 using FluentValidation;
 using SmartExpenseControl.Application.Expenses.Commands;
+using SmartExpenseControl.Domain.Repositories;
 
 namespace SmartExpenseControl.Application.Expenses.Validators;
 
 public sealed class UpdateExpenseGroupValidator : AbstractValidator<UpdateExpenseGroupCommand>
 {
-    public UpdateExpenseGroupValidator()
+    public UpdateExpenseGroupValidator(IExpenseGroupRepository expenseGroupRepository, IUserRepository userRepository)
     {
-
+        RuleFor(x => x.Id)
+            .MustAsync(async (id, _) => await expenseGroupRepository.ExistsAsync(id))
+            .WithMessage("Expense group don't exist");
+        RuleFor(x => x.UpdatedBy)
+            .MustAsync(async (id, _) => await userRepository.ExistsAsync(id))
+            .WithMessage("User doesn't exist");
+        RuleFor(x => x.Name).MaximumLength(255).NotEmpty();
+        RuleFor(x => x.Description).MaximumLength(255).NotEmpty();
     }
 }
