f44f792488985cb03e88cf9a2fd163d3998cac49
Igor Tessaro <--global>
2025-05-25 11:59:04 -0300

    feat(database): restructure expense-related tables and introduce new entities for better expense tracking

--- database/scripts/001_create_tables.sql
+++ database/scripts/001_create_tables.sql
@@ -20,39 +20,12 @@ CREATE TABLE `Users` (
     FOREIGN KEY (`role_id`) REFERENCES `UserRoles`(`id`)
 );
 
--- Category of Expenses Table
--- Home/Transport/Food/Health/Entertainment/Education/Other
-CREATE TABLE `ExpenseCategories` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL,
-    `description` TEXT
-);
-
--- Periods Table
-CREATE TABLE `Periods` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL,
-    `description` TEXT,
-    `type` ENUM('daily', 'weekly', 'bi-weekly', 'monthly', 'yearly', 'one-time') NOT NULL
-);
-
--- Period of expenses Table
-CREATE TABLE `PeriodExpenses`(
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `period_id` INT NOT NULL,
-    `expense_group_id` INT NOT NULL,
-    `start_date` DATE NOT NULL,
-    `end_date` DATE NOT NULL,
-    `status` ENUM('pending', 'open', 'locked') DEFAULT 'pending',
-    FOREIGN KEY (`period_id`) REFERENCES `Periods`(`id`),
-    FOREIGN KEY (`expense_group_id`) REFERENCES `ExpenseGroups`(`id`)
-);
-
 -- Expense Groups Table
 CREATE TABLE `ExpenseGroups` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
     `description` VARCHAR(255),
+    `periodicity` ENUM('daily', 'weekly', 'bi-weekly', 'monthly', 'yearly', 'one-time') DEFAULT 'one-time',
     `created_by` INT NOT NULL,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_by` INT,
@@ -61,36 +34,45 @@ CREATE TABLE `ExpenseGroups` (
     FOREIGN KEY (`updated_by`) REFERENCES `Users`(`id`)
 );
 
--- Payment Methods Table
--- 'pix', 'credit card', 'debit card', 'cash', 'bank transfer', 'boleto'
-CREATE TABLE `PaymentMethods` (
+CREATE TABLE `ExpenseTypes` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
-    `description` TEXT
+    `description` TEXT,
+    `expense_group_id` INT NOT NULL,
+    FOREIGN KEY (`expense_group_id`) REFERENCES `ExpenseGroups`(`id`)
+);
+
+-- Periods Table
+CREATE TABLE `ExpensePeriods` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `status` ENUM('pending', 'open', 'closed', 'locked') DEFAULT 'pending',
+    `start_date` DATE NOT NULL,
+    `end_date` DATE,
+    `expense_group_id` INT NOT NULL,
+    FOREIGN KEY (`expense_group_id`) REFERENCES `ExpenseGroups`(`id`)
 );
 
 -- Expenses Table
 CREATE TABLE `Expenses` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `period_expenses_id` INT NOT NULL,
+    `expense_period_id` INT NOT NULL,
     `name` VARCHAR(255) NOT NULL,
-    `tag` VARCHAR(100) NOT NULL,
-    `amount` DECIMAL(10, 2),
-    `payment_method_id` INT NULL,
-    `due_day` INT,
+    `amount` DECIMAL(18,2),
+    `payment_method` ENUM('pix', 'credit card', 'debit card', 'cash', 'bank transfer', 'boleto') DEFAULT NULL,
+    `due_date` DATE,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `created_by` INT NOT NULL,
     `updated_by` INT,
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `paid_by` INT,
     `paid_at` TIMESTAMP,
-    `expense_category_id` INT,
+    `expense_type_id` INT,
     FOREIGN KEY (`created_by`) REFERENCES `Users`(`id`),
     FOREIGN KEY (`updated_by`) REFERENCES `Users`(`id`),
     FOREIGN KEY (`paid_by`) REFERENCES `Users`(`id`),
-    FOREIGN KEY (`period_expenses_id`) REFERENCES `PeriodExpenses`(`id`),
-    FOREIGN KEY (`payment_method_id`) REFERENCES `PaymentMethods`(`id`),
-    FOREIGN KEY (`expense_category_id`) REFERENCES `ExpenseCategories`(`id`)
+    FOREIGN KEY (`expense_period_id`) REFERENCES `ExpensePeriods`(`id`),
+    FOREIGN KEY (`expense_type_id`) REFERENCES `ExpenseTypes`(`id`)
 );
 
 -- Expense Groups Users Table
@@ -108,50 +90,47 @@ CREATE TABLE `ExpenseGroupsUsers` (
     FOREIGN KEY (`updated_by`) REFERENCES `Users`(`id`)
 );
 
--- -- Budgets Table
--- CREATE TABLE `Budgets` (
---     `id` INT AUTO_INCREMENT PRIMARY KEY,
---     `user_id` INT NOT NULL,
---     `expense_group_id` INT NOT NULL,
---     `amount` DECIMAL(10, 2) NOT NULL,
---     `start_date` DATE NOT NULL,
---     `end_date` DATE NOT NULL,
---     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
---     `created_by` INT,
---     `updated_by` INT,
---     FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`),
---     FOREIGN KEY (`created_by`) REFERENCES `Users`(`id`),
---     FOREIGN KEY (`updated_by`) REFERENCES `Users`(`id`),
---     FOREIGN KEY (`expense_group_id`) REFERENCES `ExpenseGroups`(`id`)
--- );
+CREATE TABLE `ExpensesApportionment` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT NOT NULL,
+    `expense_group_id` INT NOT NULL,
+    `percentage` DECIMAL(5,2) NOT NULL,
+    FOREIGN KEY (`expense_group_id`) REFERENCES `ExpenseGroups`(`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`)
+);
 
--- -- Notifications Table
--- CREATE TABLE `Notifications` (
---     `id` INT AUTO_INCREMENT PRIMARY KEY,
---     `user_id` INT NOT NULL,
---     `message` TEXT NOT NULL,
---     `read` BOOLEAN DEFAULT FALSE,
---     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
---     `created_by` INT,
---     `updated_by` INT,
---     FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`),
---     FOREIGN KEY (`created_by`) REFERENCES `Users`(`id`),
---     FOREIGN KEY (`updated_by`) REFERENCES `Users`(`id`)
--- );
+CREATE TABLE `ExpensePeriodSettlement` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `expense_period_id` INT NOT NULL,
+    `total_amount` DECIMAL(18,2) NOT NULL,
+    `status` ENUM('pending', 'processing', 'settled') DEFAULT 'pending',
+    FOREIGN KEY (`expense_period_id`) REFERENCES `ExpensePeriods`(`id`)
+);
 
--- -- Expense Installments Table
--- CREATE TABLE `ExpenseInstallments` (
---     `id` INT AUTO_INCREMENT PRIMARY KEY,
---     `amount` DECIMAL(10, 2) NOT NULL,
---     `due_day` INT NOT NULL,
---     `payment_method` VARCHAR(50) NOT NULL,
---     `status` ENUM('pending', 'paid') DEFAULT 'pending',
---     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
---     `created_by` INT NOT NULL,
---     `updated_by` INT,
---     FOREIGN KEY (`created_by`) REFERENCES `Users`(`id`),
---     FOREIGN KEY (`updated_by`) REFERENCES `Users`(`id`)
--- );
+CREATE TABLE `ExpenseSettlement` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT NOT NULL,
+    `expense_period_settlement_id` INT NOT NULL,
+    `total_amount` DECIMAL(18,2) NOT NULL,
+    `payable` DECIMAL(18,2) NOT NULL,
+    `receivable` DECIMAL(18,2) NOT NULL,
+    `percentage` DECIMAL(5,2) NOT NULL,
+    `status` ENUM('pending', 'processing', 'settled') DEFAULT 'pending',
+    FOREIGN KEY (`expense_period_settlement_id`) REFERENCES `ExpensePeriodSettlement`(`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`)
+);
+
+CREATE TABLE `FinancialTransactions` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT NOT NULL,
+    `counterparty_id` INT NOT NULL,
+    `amount` DECIMAL(18,2) NOT NULL,
+    `transaction_type` ENUM('income', 'expense') NOT NULL,
+    `description` TEXT,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `status` ENUM('pending', 'completed', 'failed') DEFAULT 'pending',
+    `expense_settlement_id` INT NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`),
+    FOREIGN KEY (`counterparty_id`) REFERENCES `Users`(`id`),
+    FOREIGN KEY (`expense_settlement_id`) REFERENCES `ExpenseSettlement`(`id`)
+);
