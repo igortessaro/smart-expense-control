f17240a1dee1652c33f1fd1aef3aea4b8d16ed34
Igor Tessaro <--global>
2025-05-25 21:00:06 -0300

    refactor(project): remove unused folder references and delete Money value object

--- src/SmartExpenseControl.Domain/ValueObjects/Money.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartExpenseControl.Domain.ValueObjects;
-
-public class Money(decimal amount, string currency)
-{
-    public decimal Amount { get; private set; } = amount;
-    public string Currency { get; private set; } = currency;
-
-    // Override equality methods
-}
